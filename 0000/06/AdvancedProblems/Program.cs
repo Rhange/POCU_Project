3a488eabb22734fb31deb5ab77e1f602e3ca7dc0
Rhange <rhange0511@gmail.com>
2021-01-09 16:28:04 +0900

    Updated to Chapter 6
    condition & boolean

---
+++ 0000/06/AdvancedProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdvancedProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 입력해주세요.");
+            int userInput = int.Parse(Console.ReadLine());
+            
+            if (userInput > 20 && userInput % 3 == 0)
+            {
+                Console.WriteLine("20보다 큰 3의 배수입니다.");
+            }
+        }
+    }
+}
